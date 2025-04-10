e07ddb460b495f81148b5521c2b3b9b886af0fae
Henrique Lopes Lima <henriquelopeslima.hll@gmail.com>
2025-04-10 20:03:21 -0300

    Improve the communication by email

---
+++ src/Service/EmailService.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Service\Interface\EmailServiceInterface;
+use RuntimeException;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+readonly class EmailService implements EmailServiceInterface
+{
+    private string $fromAddress;
+
+    public function __construct(
+        private MailerInterface $mailer,
+        private ParameterBagInterface $parameterBag,
+    ) {
+        $this->fromAddress = $this->parameterBag->get('app.email.address');
+    }
+
+    public function send(
+        array $to,
+        string $subject,
+        string $content,
+    ): void {
+        if (null === $this->mailer) {
+            throw new RuntimeException('The EmailService has not been initialized. Call the EmailService::initialize() first.');
+        }
+
+        $email = (new Email())
+            ->from($this->fromAddress)
+            ->subject($subject)
+            ->html($content);
+
+        $email->to(...$to);
+
+        $this->mailer->send($email);
+    }
+
+    public function sendTemplatedEmail(
+        array $to,
+        string $subject,
+        string $htmlTemplate,
+        array $context = [],
+    ): void {
+        if (null === $this->mailer) {
+            throw new RuntimeException('EmailService was not initialize. Call the EmailService::initialize() first.');
+        }
+
+        $email = (new TemplatedEmail())
+            ->from($this->fromAddress)
+            ->subject($subject)
+            ->htmlTemplate($htmlTemplate)
+            ->context($context);
+
+        $email->to(...$to);
+
+        $this->mailer->send($email);
+    }
+}
