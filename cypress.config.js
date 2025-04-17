45d9ab523e6cf09f94a13bcb3c3495759b675a5d
Ana Kelly Moura Balbino <101601082+Akellymourab@users.noreply.github.com>
2025-04-17 15:39:30 -0300

    Adds improvments in tests cypress and github workflow. (#152)

--- cypress.config.js
+++ cypress.config.js
@@ -5,17 +5,13 @@ module.exports = defineConfig({
   viewportWidth: 1920,
   viewportHeight: 1080,
   e2e: {
+    supportFile : false,
     baseUrl: process.env.CYPRESS_BASE_URL,
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {
     },
     specPattern: [
-      'cypress/e2e/api/**/*.cy.js',
-      'cypress/e2e/web/agent/*.cy.js',
-      'cypress/e2e/web/authentication/*.cy.js',
-      'cypress/e2e/web/**/*.cy.js',
-      'cypress/e2e/admin/dashboard/*.cy.js',
-      'cypress/e2e/admin/**/*.cy.js',
+      'cypress/regmel/e2e/**/*.cy.js',
     ],
   },
 });
