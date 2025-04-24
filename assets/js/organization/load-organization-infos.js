a0471c7e4abe1e2c0e3444190c501ec003d69d37
Ana Kelly Moura Balbino <101601082+Akellymourab@users.noreply.github.com>
2025-04-23 22:51:21 -0300

    Adds improvments in modal edit company. (#177)

--- assets/js/organization/load-organization-infos.js
+++ assets/js/organization/load-organization-infos.js
@@ -1,24 +1,22 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('.edit-organization').forEach(button => {
-        button.addEventListener('click', function () {
-            const organizationId = this.getAttribute('data-id');
-            const token = this.getAttribute('data-token');
+document.addEventListener('click', function (event) {
+    if (event.target.classList.contains('edit-organization')) {
+        const organizationId = event.target.getAttribute('data-id');
+        const token = event.target.getAttribute('data-token');
 
-            fetch(`/api/organizations/${organizationId}`, {
-                headers: {
-                    'Authorization': `Bearer ${token}`
-                }
+        fetch(`/api/organizations/${organizationId}`, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                document.getElementById('edit-organization').action = `/painel/admin/municipios/${organizationId}/editar`;
+                document.getElementById('profile-img').src = data.image || 'img/city.png';
+                document.getElementById('organization-description').value = data.description ?? '';
+                document.getElementById('organization-site').value = data.extraFields?.site ?? '';
+                document.getElementById('organization-phone').value = data.extraFields?.telefone ?? '';
+                document.getElementById('organization-email').value = data.extraFields?.email ?? '';
             })
-                .then(response => response.json())
-                .then(data => {
-                    document.getElementById('profile-img').src = data.image || 'img/city.png';
-                    document.getElementById('edit-organization').action = `/painel/admin/municipios/${organizationId}/editar`;
-                    document.getElementById('organization-description').value = data.description ?? '';
-                    document.getElementById('organization-site').value = data.extraFields.site ?? '';
-                    document.getElementById('organization-phone').value = data.extraFields.phone ?? '';
-                    document.getElementById('organization-email').value = data.extraFields.email ?? '';
-                })
-                .catch(error => console.error('Error:', error));
-        });
-    });
+            .catch(error => console.error('Error:', error));
+    }
 });
