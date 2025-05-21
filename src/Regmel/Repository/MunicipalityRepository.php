0783bf73fd594049c3381b8cfba3554b7f2346d9
TalysonSoares <talysonsoares40@gmail.com>
2025-05-20 21:48:05 -0300

    Fix proposal status UpdatedAt

--- src/Regmel/Repository/MunicipalityRepository.php
+++ src/Regmel/Repository/MunicipalityRepository.php
@@ -6,14 +6,25 @@ namespace App\Regmel\Repository;
 
 use App\Entity\Organization;
 use App\Enum\StatusProposalEnum;
+use App\Environment\ConfigEnvironment;
 use App\Regmel\Repository\Interface\MunicipalityRepositoryInterface;
 use App\Repository\OrganizationRepository;
+use Doctrine\Persistence\ManagerRegistry;
 
 class MunicipalityRepository extends OrganizationRepository implements MunicipalityRepositoryInterface
 {
+    public function __construct(
+        ManagerRegistry $registry,
+        private readonly ConfigEnvironment $configEnvironment
+    ) {
+        parent::__construct($registry);
+    }
+
     public function updateProposals(Organization $organization, StatusProposalEnum $statusFrom, StatusProposalEnum $statusTo): void
     {
         $conn = $this->getEntityManager()->getConnection();
+
+        $env = $this->configEnvironment->aurora();
 
         $sql = "UPDATE initiative 
             SET 
@@ -22,7 +33,8 @@ class MunicipalityRepository extends OrganizationRepository implements Municipal
                     extra_fields::jsonb, 
                     '{status}', 
                     to_jsonb(:statusTo::text)
-                )
+                ),
+                updated_at = now() AT TIME ZONE :timezone
             WHERE 
                 extra_fields->>'city_id' = :cityId
                 AND extra_fields->>'status' = :statusFrom";
@@ -32,6 +44,7 @@ class MunicipalityRepository extends OrganizationRepository implements Municipal
             'cityId' => $organization->getExtraFields()['cityId'],
             'statusFrom' => $statusFrom->value,
             'statusTo' => $statusTo->value,
+            'timezone' => $env['default_timezone'],
         ]);
     }
 }
